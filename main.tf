fb70a92b3b39c7d307504dedeedd0e325a53258d
MoeAhmed11 <126146016+MoeAhmed11@users.noreply.github.com>
2023-02-23 14:13:46 +0000

    create dev vpc

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev_VPC"
@@ -37,7 +37,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
   subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
+
   tags = {
     Name = "HelloWorld"
   }
@@ -49,7 +49,7 @@ module "blog_sg" {
 
   name   = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks  = ["0.0.0.0/0"]
